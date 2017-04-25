26b61b3d615dc448cd680268043fcc82a165d996
atbj505 <atbj505@hotmail.com>
2017-04-25 11:18:27 +0800

    中文编码

--- NBA/api.py
+++ NBA/api.py
@@ -1,16 +1,27 @@
-import requests
 import json
+
+import requests
+
+from match import Match
 
 Match_List_Url = 'http://bifen4m.qiumibao.com/json/list.htm'
 
 
 def match_list_request(url):
     response = requests.get(url)
-    match = json.loads(response.text)
+    responseJson = json.loads(response.text)
+    matchList = list()
+    for matchJson in responseJson['list']:
+        if matchJson['type'] == 'basketball' and matchJson['period_cn'] != '完赛':
+            match = Match(**matchJson)
+            matchList.append(match)
+    return matchList
 
 
 def main():
-    match_list_request(Match_List_Url)
+    matchList = match_list_request(Match_List_Url)
+    match = matchList[0]
+    print(match)
 
 
 if __name__ == '__main__':
